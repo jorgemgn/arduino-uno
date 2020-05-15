bfce3d2da9fca698d24cb34e84d3a98e74399008
Jorge González Novez <53383025+jorgemgn@users.noreply.github.com>
2020-05-15 12:53:35 +0200

    Update and rename 4-digits.ino to 4-digits-from-serial.ino

---
+++ src/7-segment-display/4-digits-from-serial.ino
@@ -0,0 +1,6 @@
+/*
+ * Read 4 digit 7-segment display value from Serial Monitor by Jorge
+ * Based on "Arduino + Display 7 Segmentos + 4 Dígitos" by TexoLab
+ *
+ * TO-DO
+ */
--- src/7-segment-display/4-digits.ino
+++
@@ -1,4 +0,0 @@
-/*
- * 4 digit 7-segment display by Jorge
- * TO-DO
- */
